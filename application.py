46c2637df81db2b044aa1d95aa5737a84f5bbe64
Mourn <82979811+MournTHEMorning@users.noreply.github.com>
2022-11-14 08:35:42 -0500

    Nov 14: Working on Milestone 2 skeleton Rough idea for user interaction Working on logic classes

--- application.py
+++ application.py
@@ -1,2 +1,30 @@
 """APPLICATION.PY: Main module to run code"""
 #prog_assign3, application.py created
+
+#main class, user interaction
+class Program:
+    def __init__(self):
+        print("hey")
+    def showMainMenu():
+        pass
+        #has options openAccount, selectAccount and exit
+    #    if openAccount etc..
+    def showAccountMenu():
+        pass
+        #has options checkBalance, Deporsit, withdraw, exitAccount
+        #checkBalance, deposit, withdraw, exit
+    def run():
+        pass#shows main menu
+
+class Account:
+    pass #rep a bank account
+
+class SavingsAccount(Account):
+    #child of Account
+    pass
+
+
+
+
+#main code
+Program()
