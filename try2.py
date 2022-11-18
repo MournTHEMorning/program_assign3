ee6ca2540828adf90ac71188c3d8a38cc160c1bb
Mourn <82979811+MournTHEMorning@users.noreply.github.com>
2022-11-18 15:18:29 -0500

    Nov 18: Program class structure made

--- try2.py
+++ try2.py
@@ -1,1 +1,15 @@
 #attempt 2 on the program because I wrote the program incorrectly
+
+class Program:
+    def run(self):
+        #shows main menu, beginning user interaction
+        print("Welcome to Oddity Banks!\nHow can I help you?")
+        self.showMainMenu()
+        print("Have a good one, customer!")
+
+    def showMainMenu():
+        pass
+
+    def showAccountMenu():
+        pass
+#consider making seperate files for better organization
