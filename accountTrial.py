91dd68203e885dbcd28246f7d2a4fd7595d6c289
Mourn <82979811+MournTHEMorning@users.noreply.github.com>
2022-11-19 19:58:00 -0500

    Nov 19: General account class created and tested. Savings account tested (without withdraw limit)

--- accountTrial.py
+++ accountTrial.py
@@ -1,20 +1,27 @@
 #to test account and if it works
 
+#,rate_of_interest,current_Balance
+#account = culminative
 class Account:
-    def __init__(self,num,holder,rate_of_interest,current_Balance):
+    def __init__(self,num,holder,savBal,cheqBal):
         self.num=num
         self.holder=holder
-        self.roi=rate_of_interest
-        self.currBal=current_Balance
+        self.roi=0.1
+        self.curBal=savBal+cheqBal
 
+        # ChequingAccount(self.num+2,self.holder,0)
+
+    #4 digit number
     def getAccNum(self):
         return self.num
+
+    # one user    
     def getAccHolder(self):
         return self.holder
     def getROI(self):
         return self.roi
     def getCurrentBal(self):
-        return self.currBal
+        return self.curBal
 
 
     def deposit(self):
@@ -27,12 +34,19 @@ class Account:
 
 #class SavingsAccount
 class SavingsAccount(Account):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,num,holder,savBal):
+        super().__init__(num,holder,savBal,0)
+        self.roi=0.1
+
 
 
 #class ChequingAccount
 class ChequingAccount(Account):
     def __init__(self):
-        super().__init__()
-        
+        pass
+
+
+
+#main code
+a=Account(1234,"tester1",1000,5000)
+print(a.getAccHolder(), a.getAccNum(),a.getCurrentBal(),a.getROI())
