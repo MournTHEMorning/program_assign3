fb60d07f22fad3f6a106468f4c5ff091c50a44e8
Mourn <82979811+MournTHEMorning@users.noreply.github.com>
2022-11-21 14:38:57 -0500

    Nov 21: Modules updated (modules deleted and try2.py renamed to application.py)

--- application.py
+++ application.py
@@ -1,178 +1,130 @@
 """APPLICATION.PY: Main module to run code"""
+#import business logic and all classes
+import BusinessLogic
 
-#use try and except error stuff. No crashing allowed. 
-#if crashing is inevitable, tell user of error and say 'see you'
-#negative vals for bank info isn't allowed
+class Program:
+    bank=BusinessLogic.Bank()
 
-
-class Program:  #main class, user interaction
     def __init__(self):
         self.line="-----*-----"
         self.breakLine="=====* * *====="
-        self.bank=Bank()
-
-    def showAccountMenu(self,accNum):
-        userAccount=self.bank.searchAccount(accNum)
-        if(type(userAccount)==SavingsAccount or type(userAccount)==ChequingAccount):
-            print("valid!")
-        else:
-            print("falsehood")
-        #uses search in Bank to check for validity
-# 1.	Check Balance: Display the balance of the selected account
-# 2.	Deposit: Prompt the user for an amount to deposit and perform the deposit using the methods in account class. 
-# 3.	Withdraw: Prompt the user for an amount to withdraw and perform the withdrawal using the methods in the account class. 
-# 4.	Exit Account: go back to Banking Main Menu
-
-
-
-        #checkBalance, deposit, withdraw, exit
 
     def showMainMenu(self):
-        loop=True;leave=" "
-        while loop:
-            print("\nOPEN ACCOUNT [O] | SELECT ACCOUNT [S] | EXIT [EXIT]")
-            user=input("YOU SELECTED: ").upper()
+        user=" "
+        while user!="EXIT":
+            print("\nSELECT ACCOUNT [S] | EXIT [EXIT]")
             try:
-                #To open an account
-                if(user=="O"):
-                    print("open a new account")  
+                user=input("YOU SELECTED: ").upper()
 
-                #To select an account      
-                elif(user=="S"):
-                    while loop:
-                        # try:
-                        selectAcc=int(input("Fantastic. Name the number of the account (5 digit): "))
-                        self.showAccountMenu(selectAcc)
-                        #ERROR HERE; FIND OUT!!
-                        loop=False
-                        # except (ValueError, TypeError):
-                        #     print("2. Please input valid type. There was an Value Error, type or Keyboard.. error")
-                
-                #To leave Main Menu
-                elif(user=="EXIT"):
-                    print("Are you sure you want to EXIT?")  
-                    leave=input("\nType YES to confirm or anything else to deny: ").upper()
-                    if(leave=="YES"):
-                        loop=False
-                
-                #Invalid menu option
+                #Main Menu[S]: Select Account, switches to AccountMenu
+                if(user=="S"):
+                    try:
+                        acc=int(input(("Fantastic! Please write your 4-digit general account number: ")))
+                        if(acc>=1000 and acc<=9999): #checks if the acc is 4 digits, in an integer way
+
+                            #check if account exists via the method in bank
+                            askedAccount=self.bank.searchAccount(acc)
+                            if(type(askedAccount)==str):
+                                print(askedAccount)
+                            else:
+                                self.showAccountMenu(askedAccount)
+                                print("\n\nWelcome to the main menu!\n")
+
+                    except Exception:
+                        print("1. There was an error in the input")
+                    finally: 
+                        print(self.line)
+                    
+                #Main Menu: Not an option on the main menu
                 else:
-                    print("Please select a proper option...")
+                    print("Please select a proper option or type EXIT to leave.\n"+self.breakLine)
 
-            except (ValueError,EOFError,KeyboardInterrupt):
-                print("1. There was an Value Error, EOFError or Keyboard.. error")
-
+            except Exception:
+                pass
+            
             finally:
-                print(self.line)
-                if (user!="EXIT" and leave!="YES"):
-                    loop=True
-                    leave=" "
+                print(self.breakLine)
 
+    def showAccountMenu(self,askedAccount): #HERE
+        accMenu=" "
+        holderSavAcc=self.bank.searchSavAccount(askedAccount.getAccNum()) #gives program the savings account obj
+        holderCheqAcc=self.bank.searchCheqAccount(askedAccount.getAccNum()) #gives program the cheq account obj
+
+        print(self.breakLine+"\nWelcome to Account {}. This is the account menu.".format(askedAccount.getAccNum()))
+
+        while accMenu!="RETURN": 
+            print("CHECK BALANCE [B] ||| DEPOSIT [D] ||| WITHDRAW [W] ||| RETURN TO MAIN MENU [RETURN]")
+            try:
+                accMenu=input("YOU SELECTED: ").upper()
+
+                #Account Menu[B]: Checks Balance of Savings, Chequings and Culminative 
+                if(accMenu=="B"):
+                    print("Savings(Account #{}): ${} CAD\nChequing(Account #{}): ${} CAD\n----\nCulminative(Account #{}): ${} CAD".format(holderSavAcc.getAccNum(),holderSavAcc.getCurrentBal(),holderCheqAcc.getAccNum(),holderCheqAcc.getCurrentBal(),askedAccount.getAccNum(),askedAccount.getCurrentBal()))
+                    input("PRESS ANY KEY TO CONTINUE: ")
+                
+                #Account Menu[D]: Deposits money in Savings or Chequings
+                elif(accMenu=="D"):
+                    verify=input("Are you sure you want to deposit? Type YES to continue: ").upper()
+                    if (verify=="YES"):
+                        accountType=input("Deposit to Savings[S] or Chequing[C] account? ").upper()
+                        while (accountType!="S" and accountType!="C"):
+                            accountType=input("Deposit to Savings[S] or Chequing[C] account? ").upper()
+                        cash=float(input("How much money are you depositing?: "))
+                        if (accountType=="S" and cash>=0):
+                            holderSavAcc.deposit(cash)
+                            input("Your Savings deposit has been processed. PRESS ANY KEY TO CONTINUE:")
+
+                            #updates the main account's cur bal 
+                            askedAccount.deposit(cash)
+                    
+
+                        elif (accountType=="C" and cash>=0):
+                            holderCheqAcc.deposit(cash)
+                            input("Your Chequing deposit has been processed. PRESS ANY KEY TO CONTINUE:")
+                        
+                            #updates the main account's cur bal 
+                            askedAccount.deposit(cash)
+
+                #Account Menu[W]: Withdraws money in Savings or Chequings
+                elif(accMenu=="W"):
+                    verify=input("Are you sure you want to withdraw? Type YES to continue: ").upper()
+                    if (verify=="YES"):
+                        accountType=input("Withdraw from Savings[S] or Chequing[C] account? ").upper()
+                        while (accountType!="S" and accountType!="C"):
+                            accountType=input("Withdraw from Savings[S] or Chequing[C] account? ").upper()
+                        cash=float(input("How much money are you withdrawing?: "))
+                        
+                        # #SAVINGS ACCOUNT
+                        if (accountType=="S" and cash>=0):
+                            if(holderSavAcc.withdraw(cash)):
+                                input("Your Savings withdrawal has been processed. PRESS ANY KEY TO CONTINUE:")
+                                #updates the main account's cur bal 
+                                askedAccount.withdraw(cash)
+
+                        # #CHEQUING ACCOUNT
+                        elif (accountType=="C" and cash>=0):
+                            if(holderCheqAcc.withdraw(cash)):
+                                input("Your Chequing withdrawal has been processed. PRESS ANY KEY TO CONTINUE:")
+                                #updates the main account's cur bal 
+                                askedAccount.withdraw(cash)
+
+                #Account Menu: Not an option in Account Menu
+                else:
+                    if(accMenu!="RETURN"):
+                        print("Please select a proper option or type RETURN to go to main menu.\n"+self.breakLine)
+
+            except Exception:
+                print("2. There was an error in the input")
+            #ending for the account menu try block
+            finally:
+                print(self.breakLine)
+
+    #run():runs code for user interaction and begins program
     def run(self):
-        #shows main menu, beginning user interaction
         print("Welcome to Oddity Banks!\nHow can I help you?")
         self.showMainMenu()
-        print("Have a good one, customer!")
-
-class Bank: #account search/open account
-    #note to self: bank account num 5-17 digits; i'll do 5, 0.10% is standard interest or 0.001
-        #account list
-    def __init__(self):
-        self.bankName="Oddity Banks"
-        self.accountList=[]
-        #Account init:(num,holder,interest,bal): rep a bank account
-
-        #hardset accounts
-        self.acc1=SavingsAccount(12345,"Percyson Testin",0.001,10000,5000)
-        self.acc2=SavingsAccount(54321,"Reed Vers",0.001,300000,5000)
-        self.acc3=SavingsAccount(33335,"Ankil Ma",0.001,100,10000)
-        self.acc4=ChequingAccount(78910,"Notch E. Bangle",0.002,4000000,6000)
-        self.acc5=ChequingAccount(6656, "Quince Ruta",0.002,8080,500)
-
-        #appending to list
-        self.accountList.append(self.acc1)
-        self.accountList.append(self.acc2)
-        self.accountList.append(self.acc3)
-        self.accountList.append(self.acc4)
-        self.accountList.append(self.acc5)
-
-    def searchAccount(self,accN):
-        print(accN,len(self.accountList))  
-        for account in self.accountList:
-            try:
-                if (account.getAccountNum()==int(accN)):
-                    
-                    #returns an actual object associated with the account number
-                    return account
-            except (TypeError, ValueError):
-                pass
-        print("Sorry. This account does not exist")
-        
-    #method to make an account
-    def openAccount():
-        pass#open seasame~
-
-class Account: #rep a bank account
-    #note to self: bank account num 5-17 digits; i'll do 5, 0.10% is standard interest or 0.001
-    def __init__(self,num,holder,interest,bal): 
-        self.accNum=num #int val
-        self.accHolderName=holder #string val
-        self.rateOfInterest=interest #float val
-        self.currentBalance=bal #int val
-
-    def getAccountNum(self):
-        return self.accNum
-        
-    def getAccountHolderName(self):
-        return self.getAccountHolderName
-
-    def getRateOfInterest(self):
-        return self.rateOfInterest
-
-    def getCurrentBalance(self):
-        return self.currentBalance
-
-    #adds deposit to currentBalance
-    def deposit(self,dep):
-        self.currentBalance+=dep
-    
-    #removes requested amount if holder has amount and gives it to holder
-    def withdraw(self,want):
-        if(self.currentBalance-want>=0.01 and want>=0.01):
-            self.currentBalance-=want
-            return want
-        print("You limit funds to withdraw ${}.".format(self.currentBalance))
-        return 0
-    
-class SavingsAccount(Account):  #child of Account, which restricts withdrawal in account
-
-    def __init__(self,num,holder,interest,bal,mini):
-        super().__init__(num,holder,interest,bal)
-        self.minimumBalance=mini
-
-    def withdraw(self,want):
-        #note to self: check if this works
-        if((self.currentBalance-want)>=self.minimumBalance):
-            self.currentBalance-=want
-            return want
-        #else statement
-        print("You limit funds to withdraw ${}.".format(want))
-        return 0   
-
-class ChequingAccount(Account): #child of Account. transactions cannot extend over a given limit
-    def __init__(self,num,holder,interest,bal,ovDAllow):
-        super().__init__(num,holder,interest,bal)
-        self.overdraftAllowed=ovDAllow
-
-    def withdraw(self, want):
-        if(want<=self.getCurrentBalance()+self.overdraftAllowed):
-            self.currentBalance-=want
-            return want
-          #else statement
-        print("You limit funds to withdraw ${}.".format(want))
-        return 0   
-
+        print("\nHave a good one, customer!")
 
 #main code
-application=Program()
-application.run()
+main=Program()
+main.run()
