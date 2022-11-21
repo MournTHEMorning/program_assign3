c19ef42400eb9a706de1936deb3a0453d4fbea89
Mourn <82979811+MournTHEMorning@users.noreply.github.com>
2022-11-21 14:02:23 -0500

    Nov 21: chequing withdrawal algorithm tested and completed to be copied into program and tested

--- withdrawal_test.py
+++ withdrawal_test.py
@@ -1,19 +1,28 @@
-
-
 #THE MAIN ANNOYANCE RIGHT NOW -withdrawal HERE!!!
-def withdraw(self,requestedAmount):
-    self.curBal=0
-    self.currentOverdraft=0
+def withdraw(requestedAmount):
+    curBal=-10
+    overdraft=0
 
-    if (requestedAmount>=0 and (requestedAmount>=(self.curBal+self.currentOverdraft) and (self.currentOverdraft<=0 and self.curBal<0))):
-        print("Withdraw rejected. Your current balance is ${} CAD, and you have ${} CAD left in your overdraft.".format(self.curBal,self.currentOverdraft))
+    # if (requestedAmount>=0 and (requestedAmount>=(curBal+overdraft) and (overdraft<=0 and curBal<0))):
+    if(requestedAmount<0 or (requestedAmount>overdraft)):
+        print("Withdraw rejected. Your current balance is ${} CAD, and you have ${} CAD left in your overdraft.".format(curBal,overdraft))
+        print("overdraft {} | bal {} | request {}".format(overdraft,curBal,requestedAmount))
+
         return False #False for successful print statement to show or not
+    
     else:
-        if(self.curBal<=0):
-            self.currentOverdraft-=(requestedAmount)
-        self.curBal-=requestedAmount
+        if(curBal>0):
+            curBal-=requestedAmount
+            if(curBal<=0):
+                overdraft-=abs(curBal)
+        #if curBal is less than or greater than zero
+        else:
+            curBal-=requestedAmount
+            overdraft-=requestedAmount
 
         print("oui oui oui cheq")
+        print("overdraft {} | bal {} | request {}".format(overdraft,curBal,requestedAmount))
+
         return True
 
 withdraw(10)
